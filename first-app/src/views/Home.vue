212c7acc5819a8548c38822ac60cc7da616e0e31
gab <gabdez98@gmail.com>
2019-01-07 00:01:13 +0100

    begin app with vuejs and ionic!

---
+++ first-app/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <ion-checkbox checked></ion-checkbox>
+      <ion-item>
+        <ion-label position="floating">Floating Label</ion-label>
+        <ion-input class="red"></ion-input>
+      </ion-item>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
